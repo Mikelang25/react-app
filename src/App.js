15c9fab46debd8ba9c75894f93136e51aa1f45ca
unknown <michaellang2525@yahoo.com>
2020-03-10 14:02:38 -0400

    marking selected characters is working, score and guess status are incrementing and appearing

--- src/App.js
+++ src/App.js
@@ -6,18 +6,42 @@ import characters from "./characters.json";
 class App extends React.Component {
 
   state = {
-    characters
+    characters,
+    answer: "",
+    selected:[],
+    score:0
   };
 
+  shuffleCharacters(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
 
+  markedSelected = id =>{
+      if(this.state.selected.includes(id)){
+        this.setState({
+          answer:"That guess is wrong!",
+          selected:[],
+          score:0
+        });
+      }else{
+        this.state.selected.push(id)
+        this.setState({
+          answer:"That guess is correct!",
+          score: this.state.score + 1
+        });
+      }
+  }
 
   render() {
     return (
       <div className="wrapper">
         <div className="row">
           <div className="col-md-4 text-center header">Clicky Game</div>
-          <div className="col-md-4 text-center header">Guess Status</div>
-          <div className="col-md-4 text-center header">Scoreboard</div>
+          <div className="col-md-4 text-center header"><p>{this.state.answer}</p></div>
+    <div className="col-md-4 text-center header"><p>Score: {this.state.score}</p></div>
         </div>
         <div className="row">
           <div className="col-md-3 text-center dir-container">
@@ -25,14 +49,16 @@ class App extends React.Component {
             <p>Click on the images to earn points, but don't click the same one twice!</p>
           </div>
           <div className="col-md-9 char-container">
-            <div class="row">
+            <div className="row">
               <div id="characters" className="col-md-11">
+                {this.shuffleCharacters(characters)}
                 {this.state.characters.map(character => (
                   <CharacterCard
-                      name = {character.name}
-                      key={character.id}
-                      id={character.id}
-                      image={character.image}
+                    name={character.name}
+                    key={character.id}
+                    id={character.id}
+                    image={character.image}
+                    markedSelected = {this.markedSelected}
                   />
                 ))}
               </div>
--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 
+const styles = {
+    containerStyle: {
+      float: "left",
+      margin:"20px",
+      border: "1pt solid black",
+      width:"205px",
+      height:"205px",
+      textAlign: "center",
+      borderRadius:"5px"
+    },
+    image:{
+        marginTop:"3px"
+    }
+  };
 
 function CharacterCard(props){
     return (
-        <div>
-            <img alt={props.name} src={props.image}/>
-            {props.name}
+        <div style={styles.containerStyle} onClick={() => props.markedSelected(props.id)}>
+            <img style={styles.image} alt={props.name} src={props.image} id={props.id}/>
         </div>
     );    
 }
