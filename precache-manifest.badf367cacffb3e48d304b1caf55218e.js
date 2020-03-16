1ed070f2bf57a98069e3834efaa377d35e266639
unknown <michaellang2525@yahoo.com>
2020-03-16 15:55:22 -0400

    Updates

---
+++ precache-manifest.badf367cacffb3e48d304b1caf55218e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d202e84788de1f0baf574338e89df4",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "6da28e53468bc7ef9d32",
+    "url": "/react-app/static/css/main.702d403a.chunk.css"
+  },
+  {
+    "revision": "ae4586abe754eecabf3e",
+    "url": "/react-app/static/js/2.890d2658.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-app/static/js/2.890d2658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da28e53468bc7ef9d32",
+    "url": "/react-app/static/js/main.aa4f98b7.chunk.js"
+  },
+  {
+    "revision": "e1583ad798e34d126b06",
+    "url": "/react-app/static/js/runtime-main.08728517.js"
+  },
+  {
+    "revision": "7c2eac8723aa1d62fc78876d36bb3833",
+    "url": "/react-app/static/media/funky-lines.7c2eac87.png"
+  },
+  {
+    "revision": "bfc9b983b4028011805677d7854507e9",
+    "url": "/react-app/static/media/tweed.bfc9b983.png"
+  }
+]);
